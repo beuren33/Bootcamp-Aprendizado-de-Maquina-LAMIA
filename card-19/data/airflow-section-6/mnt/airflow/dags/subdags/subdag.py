62ca86aefb12c0b6a87ed1fb7e5d0d2953742017
Matheus Beuren <matheusbeuren@gmail.com>
2026-08-18 16:25:10 -0300

    fix: problem dockerfile

--- card-19/data/airflow-section-6/mnt/airflow/dags/subdags/subdag.py
+++ card-19/data/airflow-section-6/mnt/airflow/dags/subdags/subdag.py
@@ -5,6 +5,7 @@ def factory_subdag(parent_dag_name, child_dag_name, default_args):
 
     with DAG(
         dag_id='%s.%s' % (parent_dag_name, child_dag_name),
+        # cria um i com base no dag pai e no dag  filho
         default_args=default_args
     ) as dag:
 
@@ -12,5 +13,5 @@ def factory_subdag(parent_dag_name, child_dag_name, default_args):
             DummyOperator(
                 task_id='%s-task-%s' % (child_dag_name, i + 1)
             )
-
+    # sera criado 5 tasks que vao rodar em paralelo pois nao há nenhuma ordem de dependencia
     return dag
--- card-19/data/airflow-section-6/mnt/airflow/dags/test_subdag.py
+++ card-19/data/airflow-section-6/mnt/airflow/dags/test_subdag.py
@@ -6,10 +6,11 @@ from airflow.operators.subdag_operator import SubDagOperator
 from airflow.executors.sequential_executor import SequentialExecutor
 from airflow.executors.celery_executor import CeleryExecutor
 
-DAG_NAME="test_subdag"
+DAG_NAME="subdag_teste"
+# define uma constante com o nome do dag
 
 default_args = {
-    'owner': 'Airflow',
+    'owner': 'matheus',
     'start_date': airflow.utils.dates.days_ago(2)
 }
 
@@ -18,16 +19,20 @@ with DAG(dag_id=DAG_NAME, default_args=default_args, schedule_interval="@once") 
         task_id='start'
     )
 
+    # cria um subdag do dag pai
     subdag_1 = SubDagOperator(
         task_id='subdag-1',
         subdag=factory_subdag(DAG_NAME, 'subdag-1', default_args),
+        # nome dag pai | nome subdag | pega os argumentos padrao definidos la em cima
         executor=SequentialExecutor()
     )
 
+    # task vazia
     some_other_task = DummyOperator(
         task_id='check'
         )
 
+    #cria o segundo sub-dag
     subdag_2 = SubDagOperator(
         task_id='subdag-2',
         subdag=factory_subdag(DAG_NAME, 'subdag-2', default_args),
@@ -37,5 +42,6 @@ with DAG(dag_id=DAG_NAME, default_args=default_args, schedule_interval="@once") 
     end = DummyOperator(
         task_id='final'
     )
-
+    
+    # dependencias dos sub-dags
     start >> subdag_1 >> some_other_task >> subdag_2 >> end
